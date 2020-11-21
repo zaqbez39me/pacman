546db43bbb28e9dc7873b778fa7bfb2e3b9c7892
Makar Kuleshov <kulmak41@gmail.com>
2020-11-21 20:37:08 +0300

    Fix for using new classes

--- objects/__init__.py
+++ objects/__init__.py
@@ -2,3 +2,8 @@ from .ball import BallObject
 from .button import ButtonObject
 from .image import ImageObject
 from .text import TextObject
+from .ghost import Ghost
+from .player import Player
+from .wall import Wall
+from .lives import Lives
+
