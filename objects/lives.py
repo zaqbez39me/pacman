8dea1d77ba2f02914fcc0c78fb9ed14c060024b6
Makar Kuleshov <kulmak41@gmail.com>
2020-11-21 20:59:25 +0300

    Fix of names of files and names of functions

--- objects/lives.py
+++ objects/lives.py
@@ -4,10 +4,10 @@ import pygame as pg
 class Lives:
     MAX_LIVES = 2
 
-    def __init__(self, size, screen):
-        self.screen = screen
+    def __init__(self, game):
+        self.game = game
         self.lives = 2
-        self.img = pg.image.load('pacman_0.png')
+        self.img = pg.image.load('images/pacman_0.png')
         self.width = 30
         self.height = 30
         self.img = pg.transform.scale(self.img, (self.width, self.height))
@@ -19,5 +19,5 @@ class Lives:
         for i in range(self.lives):
             self.rect.x = x
             self.rect.y = y
-            self.screen.blit(self.img, self.rect)
+            self.game.screen.blit(self.img, self.rect)
             x += self.width
